79e5c034245e2830f5ec18bdd863ae3a961b9e60
damianolszewski <44201801+damianolszewski@users.noreply.github.com>
2023-03-19 00:04:38 +0100

    Create voice_utils.py

---
+++ bot/utils/voice_utils.py
@@ -0,0 +1,9 @@
+import discord
+
+async def get_voice_channel_members(interaction: discord.Interaction) -> list[discord.Member]:
+    """Returns a list of members in the voice channel the user is currently in."""
+    if not interaction.user.voice:
+        return []
+
+    voice_channel = interaction.user.voice.channel
+    return [member for member in voice_channel.members if not member.bot]

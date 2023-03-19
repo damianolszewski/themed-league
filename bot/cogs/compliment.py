aebac35d8a44a4bc8abc20620393efe72a2dc968
damianolszewski <44201801+damianolszewski@users.noreply.github.com>
2023-03-19 15:04:32 +0100

    Update compliment.py

--- bot/cogs/compliment.py
+++ bot/cogs/compliment.py
@@ -61,7 +61,11 @@ class Compliment(commands.Cog):
                 print("Using google tts")
                 # Use the Google Text-to-Speech library to generate an audio clip of the message being spoken
                 synthesis_input = texttospeech.SynthesisInput(text=compliment)
-                voice = texttospeech.VoiceSelectionParams(language_code="pl-PL", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
+                voice = texttospeech.VoiceSelectionParams(
+                    language_code="pl-PL", 
+                    ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+                    name="pl-PL-Wavenet-A"
+                )
                 audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
 
                 response = tts_client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
